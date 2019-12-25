88ec872118c1d2f425e74bdffdc3ba47aea9190d
mamananalyzer <dbasedown211@gmail.com>
2019-12-25 18:21:43 +0700

    membuat navbar, fixed

--- 10project/face.php
+++ 10project/face.php
@@ -9,69 +9,61 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
 
-    <title>Hello, world!</title>
+    <title>Amptron Instrumindo</title>
   </head>
   <body>
-  	<div>
-      <br>
-      <br>
-	  
-
-	  <div class="row fixed-top no-gutters">
-	    <div class="col">
+	     <div class="row fixed-top no-gutters font-weight-bold ">
+	     <div class="col ">
 	      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-		      <a class="navbar-brand" href="#">
-            <img src="img/aii.png" width="50" height="50" class="d-inline-block align-top" alt="">
-          </a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item active">
-		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Link</a>
-		      </li>
-		      <li class="nav-item dropdown">
-		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          Dropdown
-		        </a>
-		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		          <a class="dropdown-item" href="#">Action</a>
-		          <a class="dropdown-item" href="#">Another action</a>
-		          <div class="dropdown-divider"></div>
-		          <a class="dropdown-item" href="#">Something else here</a>
-		        </div>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-		      </li>
-		    </ul>
-		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-		    </form>
-		  </div>
-		</nav>
+    		      <a class="navbar-brand" href="#">
+                <img src="img/aii.png" width="90" height="90" class="d-inline-block align-top rounded-circle" alt="">
+              </a>
+    		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    		    <span class="navbar-toggler-icon"></span>
+    		  </button>
+    		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    		    <ul class="navbar-nav mr-auto">
+    		      <li class="nav-item active">
+    		        <a class="nav-link" href="#">BERANDA <span class="sr-only">(current)</span></a>
+    		      </li>
+    		      <li class="nav-item">
+    		        <a class="nav-link" href="#">TENTANG KAMI</a>
+    		      </li>
+    		      <li class="nav-item dropdown">
+    		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    		          PRODUK
+    		        </a>
+    		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    		          <a class="dropdown-item" href="#">Action</a>
+    		          <a class="dropdown-item" href="#">Another action</a>
+    		          <div class="dropdown-divider"></div>
+    		          <a class="dropdown-item" href="#">Something else here</a>
+    		        </div>
+    		      </li>
+    		      <li class="nav-item">
+    		        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">KONTAK</a>
+    		      </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">BERITA</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">SERVIS</a>
+              </li>
+    		    </ul>
+    		    <form class="form-inline my-2 my-lg-0">
+    		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    		      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    		    </form>
+    		  </div>
+		    </nav>
 	    </div>
-
-	    <!-- <div class="col-2">
-	      <h1>yuhu</h1>
-	    </div> -->
-
+      
 	  </div>
-	</div>
-
-    
-
-    
-    
-
-    <br>
-    <br>
-    <div class="jumbotron container jumbotron-fluid">
+      <br>
+      <br>
+      <br>
+      <br>
+    <div class="jumbotron jumbotron-fluid">
       <div class="container text-center">
         <img src="img/aii.png" width="200" class="rounded-circle">
         <h1 class="display-3">Amptron Instrumindo</h1>
